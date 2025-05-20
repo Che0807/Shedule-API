b22689ab04839e849ac1d2b2b79b52686c87ba6c
Che0807 <elpin0428@naver.com>
2025-05-20 19:11:02 +0900

    Initial commit

---
+++ src/main/java/com/example/sheduleapi/SheduleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheduleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheduleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sheduleapi/SheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
